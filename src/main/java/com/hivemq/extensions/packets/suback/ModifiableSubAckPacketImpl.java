f36467d420a71f11b5f710c93eecc41e1fb816a2
ratherto <robinatherton1995@gmail.com>
2019-10-09 16:33:42 +0200

    Added tests for modifying SUBACK packages.

--- src/main/java/com/hivemq/extensions/packets/suback/ModifiableSubAckPacketImpl.java
+++ src/main/java/com/hivemq/extensions/packets/suback/ModifiableSubAckPacketImpl.java
@@ -1,32 +1,37 @@
 package com.hivemq.extensions.packets.suback;
 
+import com.google.common.base.Preconditions;
 import com.hivemq.configuration.service.FullConfigurationService;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.extension.sdk.api.packets.general.ModifiableUserProperties;
 import com.hivemq.extension.sdk.api.packets.suback.ModifiableSubAckPacket;
 import com.hivemq.extension.sdk.api.packets.subscribe.SubackReasonCode;
 import com.hivemq.extensions.packets.general.ModifiableUserPropertiesImpl;
+import com.hivemq.extensions.services.builder.PluginBuilderUtil;
 import com.hivemq.mqtt.message.reason.Mqtt5SubAckReasonCode;
 import com.hivemq.mqtt.message.suback.SUBACK;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Robin Atherton
  */
 public class ModifiableSubAckPacketImpl implements ModifiableSubAckPacket {
 
+    private final @NotNull FullConfigurationService configurationService;
     private final @NotNull ModifiableUserPropertiesImpl userProperties;
     private final int packetIdentifier;
 
     private boolean modified = false;
     private String reasonString;
-    private @NotNull List<SubackReasonCode> subAckReasonCodes;
+    private @NotNull List<SubackReasonCode> reasonCodes;
 
     public ModifiableSubAckPacketImpl(
             final @NotNull FullConfigurationService fullConfigurationService,
             final @NotNull SUBACK subAck) {
+        this.configurationService = fullConfigurationService;
         this.userProperties = new ModifiableUserPropertiesImpl(
                 subAck.getUserProperties().getPluginUserProperties(),
                 fullConfigurationService.securityConfiguration().validateUTF8());
@@ -36,7 +41,7 @@ public class ModifiableSubAckPacketImpl implements ModifiableSubAckPacket {
         for (final Mqtt5SubAckReasonCode code : subAck.getReasonCodes()) {
             subAckReasonCodes.add(SubackReasonCode.valueOf(code.name()));
         }
-        this.subAckReasonCodes = subAckReasonCodes;
+        this.reasonCodes = subAckReasonCodes;
     }
 
     public boolean isModified() {
@@ -45,24 +50,28 @@ public class ModifiableSubAckPacketImpl implements ModifiableSubAckPacket {
 
     @Override
     public void setReasonString(final @NotNull String reasonString) {
+        Preconditions.checkNotNull(reasonString, "Reason string must never be null");
+        PluginBuilderUtil.checkReasonString(reasonString, configurationService.securityConfiguration().validateUTF8());
         this.reasonString = reasonString;
         this.modified = true;
     }
 
     @Override
-    public void setReasonCodes(
-            final List<SubackReasonCode> reasonCodes) {
-        if (this.subAckReasonCodes.size() < reasonCodes.size()) {
-            //TODO
+    public void setReasonCodes(final @NotNull List<SubackReasonCode> reasonCodes) {
+        Preconditions.checkNotNull(reasonCodes, "Reason string must never be null");
+        if (Objects.equals(this.reasonCodes, reasonCodes)) {
             return;
         }
-        this.subAckReasonCodes = reasonCodes;
+        if (reasonCodes.size() != this.reasonCodes.size()) {
+            throw new IllegalArgumentException("You cannot change the amount of reason codes.");
+        }
+        this.reasonCodes = reasonCodes;
         this.modified = true;
     }
 
     @Override
     public List<SubackReasonCode> getReasonCodes() {
-        return subAckReasonCodes;
+        return reasonCodes;
     }
 
     @Override
---
+++ src/test/java/com/hivemq/extensions/packets/suback/ModifiableSubAckPacketImplTest.java
@@ -0,0 +1,126 @@
+package com.hivemq.extensions.packets.suback;
+
+import com.hivemq.configuration.service.FullConfigurationService;
+import com.hivemq.extension.sdk.api.packets.subscribe.SubackReasonCode;
+import com.hivemq.mqtt.message.mqtt5.Mqtt5UserProperties;
+import com.hivemq.mqtt.message.mqtt5.Mqtt5UserPropertiesBuilder;
+import com.hivemq.mqtt.message.mqtt5.MqttUserProperty;
+import com.hivemq.mqtt.message.reason.Mqtt5SubAckReasonCode;
+import com.hivemq.mqtt.message.suback.SUBACK;
+import org.junit.Before;
+import org.junit.Test;
+import util.TestConfigurationBootstrap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ModifiableSubAckPacketImplTest {
+
+    private ModifiableSubAckPacketImpl packet;
+
+    private SUBACK original;
+
+    private FullConfigurationService configurationService;
+    private List<Mqtt5SubAckReasonCode> originalReasonCodes;
+    private List<SubackReasonCode> modifiedReasonCodes;
+
+    @Before
+    public void setUp() throws Exception {
+        originalReasonCodes = new ArrayList<>();
+        originalReasonCodes.add(Mqtt5SubAckReasonCode.PACKET_IDENTIFIER_IN_USE);
+        originalReasonCodes.add(Mqtt5SubAckReasonCode.IMPLEMENTATION_SPECIFIC_ERROR);
+        originalReasonCodes.add(Mqtt5SubAckReasonCode.QUOTA_EXCEEDED);
+        original = createTestSubAck(1, originalReasonCodes, "reasonCodes");
+        packet = createTestSubAckPacket(1, originalReasonCodes, "reasonCodes");
+
+        modifiedReasonCodes = new ArrayList<>();
+        modifiedReasonCodes.add(SubackReasonCode.GRANTED_QOS_1);
+        modifiedReasonCodes.add(SubackReasonCode.IMPLEMENTATION_SPECIFIC_ERROR);
+        modifiedReasonCodes.add(SubackReasonCode.NOT_AUTHORIZED);
+    }
+
+    @Test
+    public void test_change_all_valid_values() {
+        final List<SubackReasonCode> reasonCodes = new ArrayList<>();
+        reasonCodes.add(SubackReasonCode.GRANTED_QOS_1);
+        reasonCodes.add(SubackReasonCode.IMPLEMENTATION_SPECIFIC_ERROR);
+        reasonCodes.add(SubackReasonCode.NOT_AUTHORIZED);
+
+        packet.setReasonString("testReasonString");
+        packet.setReasonCodes(reasonCodes);
+
+        assertEquals("testReasonString", packet.getReasonString());
+        assertEquals(SubackReasonCode.GRANTED_QOS_1, packet.getReasonCodes().get(0));
+        assertEquals(SubackReasonCode.IMPLEMENTATION_SPECIFIC_ERROR, packet.getReasonCodes().get(1));
+        assertEquals(SubackReasonCode.NOT_AUTHORIZED, packet.getReasonCodes().get(2));
+    }
+
+    @Test
+    public void test_modify_packet() {
+        packet = new ModifiableSubAckPacketImpl(configurationService, original);
+        packet.setReasonCodes(modifiedReasonCodes);
+        assertTrue(packet.isModified());
+
+        packet = new ModifiableSubAckPacketImpl(configurationService, original);
+        packet.setReasonString("testTestTest");
+        assertTrue(packet.isModified());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test_set_reason_string_null() {
+        packet.setReasonString(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test_set_reason_codes_null() {
+        packet.setReasonCodes(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void reasonString_invalid_input() {
+        packet.setReasonString("topic" + '\u0001');
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void reasonString_exceeds_max_length() {
+        final StringBuilder s = new StringBuilder("s");
+        for (int i = 0; i < 65535; i++) {
+            s.append("s");
+        }
+        packet.setReasonString(s.toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_reason_codes_with_different_sizes() {
+        final ArrayList<SubackReasonCode> subackReasonCodes = new ArrayList<>();
+        subackReasonCodes.add(SubackReasonCode.GRANTED_QOS_0);
+        subackReasonCodes.add(SubackReasonCode.SHARED_SUBSCRIPTION_NOT_SUPPORTED);
+        packet.setReasonCodes(subackReasonCodes);
+    }
+
+    private ModifiableSubAckPacketImpl createTestSubAckPacket(
+            final int packetIdentifier,
+            final List<Mqtt5SubAckReasonCode> reasonCodes,
+            final String reasonString) {
+        configurationService = new TestConfigurationBootstrap().getFullConfigurationService();
+        final Mqtt5UserPropertiesBuilder builder =
+                Mqtt5UserProperties.builder().add(new MqttUserProperty("test", "test"));
+        final Mqtt5UserProperties properties = builder.build();
+        final SUBACK suback = new SUBACK(packetIdentifier, reasonCodes, reasonString, properties);
+        return new ModifiableSubAckPacketImpl(configurationService, suback);
+    }
+
+    private SUBACK createTestSubAck(
+            final int packetIdentifier,
+            final List<Mqtt5SubAckReasonCode> reasonCodes,
+            final String reasonString) {
+        configurationService = new TestConfigurationBootstrap().getFullConfigurationService();
+        final Mqtt5UserPropertiesBuilder builder =
+                Mqtt5UserProperties.builder().add(new MqttUserProperty("test", "test"));
+        final Mqtt5UserProperties properties = builder.build();
+        return new SUBACK(packetIdentifier, reasonCodes, reasonString, properties);
+    }
+}
